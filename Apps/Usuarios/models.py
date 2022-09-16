a6afbdecc5bc9876bb2b6e050b2f534a738ad313
DabloDV <juanpabloduquev@hotmail.com>
2022-09-16 01:33:25 -0500

    Inicio de modelado de usuarios

--- Apps/Usuarios/models.py
+++ Apps/Usuarios/models.py
@@ -1,3 +1,22 @@
+from tkinter import Widget
 from django.db import models
+from django import forms
 
 # Create your models here.
+
+
+
+# Crear clase con el nombre de la tabla. Tener parámetros: models.Model
+# Esto representa las tablas con sus atributos, donde models.tipodeatributo
+
+class Usuarios(models.Model):
+    idUsuario=models.Autofield(primary_key=True)
+    documento=models.CharField(max_length=10)
+    nUsuario= models.CharField(max_length=20)
+    contrasena= models.CharField(max_length=20, Widget=forms.PasswordInput)
+    correo= models.EmailField(blank=False,null=False)
+    fechaCreacion= models.DateTimeField(auto_now=True) 
+    #estado= models. 
+    #idRol= models.
+
+    #Makemigrations, y luego migrate
--- SadcomStetic/settings.py
+++ SadcomStetic/settings.py
@@ -113,8 +113,12 @@ WSGI_APPLICATION = 'SadcomStetic.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'usuariosprueba',
+        'USER': 'root',
+        'PASSWORD': '1234',
+        'HOST': 'localhost',
+        'PORT': '3305',
     }
 }
 
